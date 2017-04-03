4d6100c22561278ccbdf80dc15e1ecc6e7ad68a2
ElNelyo <charly.strn@gmail.com>
2017-04-03 21:09:36 +0200

    Add files via upload

---
+++ Object/mainObject.inc.php
@@ -0,0 +1,14 @@
+<?php include_once'activite.inc.php';
+include_once'articles.inc.php';
+include_once'biographie.inc.php';
+include_once'grade.inc.php';
+include_once'joursemaine.inc.php';
+include_once'langues.inc.php';
+include_once'main_settings.inc.php';
+include_once'membre.inc.php';
+include_once'planning.inc.php';
+include_once'prestation.inc.php';
+include_once'rel_planning_joursemaine.inc.php';
+include_once'reseauxsociaux.inc.php';
+include_once'sponsor.inc.php';
+?>
