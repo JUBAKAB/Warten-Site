bcf184d0f490439b9b0ee6fb088335461ae89016
Charly Stern <Charly Stern>
2017-04-07 20:54:25 +0200

    Autoload fb

--- test.php
+++ test.php
@@ -66,5 +66,12 @@ print_r($Examplereseauxsociaux->get_reseauxsociaux());echo'</br>';
 		echo'</br>';
 print_r($Examplesponsor->get_sponsor());echo'</br>';
 		
+
+
+
+require_once __DIR__ . '/API/facebook-sdk-v5/autoload.php';
+
+
+
 ?></body>
 </html>
