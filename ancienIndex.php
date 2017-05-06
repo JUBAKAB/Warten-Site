5f1135d83d718f0fe09d1712f6866c84f5265ac2
Charly Stern <Charly Stern>
2017-05-06 14:02:12 +0200

    ex-index

---
+++ ancienIndex.php
@@ -0,0 +1,20 @@
+<?php 
+echo'<a href="test.php"/>Afficher les données brutes</a>';?>
+<h1>Parcours des pages </h1>
+
+
+
+<a href="?link=accueil">Accueil</a>
+<a href="?link=sponsor">Sponsor</a>
+
+<br>
+<br>
+
+
+
+<?php include_once('Controleurs/mainControler.php');?>
+
+
+
+
+
