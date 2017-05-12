f0f24b0fe5ed0c5e8b4cffdf96db0f083c2f468a
Charly Stern <Charly Stern>
2017-05-12 14:37:06 +0200

    fix slider

--- View/Accueil.php
+++ View/Accueil.php
@@ -1,18 +1,32 @@
- 
-        <!--Slider-->
-        <div class="slider_container">
-            
-        </div>
-        <div class="background_image"></div>
-        <!-- Cellules -->
-        <div class="cellule_container">
-           <div class="center_image">
-            <div class="cellule_articles">
-            </div>
-            <div class="cellule_activity">
-            </div>
-            </div>
-        </div>
-    </body>
+<!--Slider-->
+ <div class="slider_container">
+     <div class="sliderbox">
+            <?php
+            for($i = 0; $i <$nombreSlider;$i++){
+             echo '<div class="box_img" style="z-index:'.$i.'">
+                 <a href="'.$lienSlider[$i].'">
+                     <img class="img_slider" src="img/img_slider'. $i.'.png">
+                     </img>
+              </a>
+           </div>';
+              
+             }
+             ?>
+    </div>
 
-    </html>
+</div>
+ <div class="background_image"></div>
+ <!-- Cellules -->
+ <div class="cellule_container">
+     <div class="center_image">
+        <div class="cellule_articles">
+          </div>
+
+    <div class="cellule_activity">
+          </div>
+          </div>
+ </div>
+
+ 
+ </body>
+ </html>
