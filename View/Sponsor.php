47e592635b77d42c6806e04a487ed380f572338a
Charly Stern <Charly Stern>
2017-05-12 14:37:19 +0200

    Fix inutile

--- View/Sponsor.php
+++ View/Sponsor.php
@@ -1,28 +1,9 @@
-<!-- Ceci est le slider.... Et pas les sponsors --> 
 
-    <!--Slider-->
-        <div class="slider_container">
-            
-        </div>
-        
-        <!-- Cellules -->
-        <div class="cellule_container">
-           <div class="center_image">
-            <div class="cellule_articles">
                     <?php
-                                // ????? Pas de JS  
+                                
                                 foreach ($unSponsor as $LesSponsors) {
                                     echo $unSponsor->get_nom();
                                     echo $unSponsor->get_texte_fr();
                                 }
 
-                    ?>
-            </div>
-            <div class="cellule_activity">
-
-            </div>
-            </div>
-        </div>
-    </body>
-
-    </html>
+  ?>
