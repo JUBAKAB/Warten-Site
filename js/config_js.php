35207f4cec12153be60a9afe034d9ba7871bc738
Charly Stern <Charly Stern>
2017-05-12 14:40:04 +0200

    fix

--- js/config_js.php
+++ js/config_js.php
@@ -6,7 +6,6 @@ echo "let javascript_array = ". $js_array . ";\n";
 ?>
 const imgSlider = document.getElementsByClassName("img_slider");
 animateSlider();
-
 // JS hover autogeneration
 function changeImage(imageSource,i,choose){ // Image nom , incrementation de la boucle,choix entre hover et nonHover
     if(choose){
@@ -34,6 +33,4 @@ for(var i = 0; i< imgSlider.length;i++)
         }
     }
 }
-
-
 </script>
