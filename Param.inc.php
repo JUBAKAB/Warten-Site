2b061cd799c9aeb137ed7683909c87ae09f5c6cc
JulianBoi <boi.julian@yahoo.com>
2017-05-14 14:12:12 +0200

    Correctif

--- Param.inc.php
+++ Param.inc.php
@@ -3,10 +3,7 @@
 class Param{
     public static $login="root";
     public static $mdp="";
-<<<<<<< HEAD
     public static $dsn = 'mysql:host=localhost:3306;dbname=warten;charset=utf8';
-=======
-    public static $dsn = 'mysql:host=localhost;dbname=warten;charset=utf8';
->>>>>>> 7e8dad7806b5a8f9966845a7de77bafd96e9e064
+//    public static $dsn = 'mysql:host=localhost;dbname=warten;charset=utf8';
 }
 ?>
