e8dfb4f96094817bcb41974f928bbb7100f51ef2
JulianBoi <boi.julian@yahoo.com>
2017-05-20 11:06:15 +0200

    Footer et Biographie rajouté + Commentaire

--- View/Accueil.php
+++ View/Accueil.php
@@ -20,15 +20,49 @@
         <div class="backgroundcellules">
             <span class="titleCellules">Articles</span>
         </div>
+         <!-- Contenu des articles -->
+        <!--         
+           Ici il y'aura des photos des articles avec le lien de l'articles pour chaque articles  
+           Procédé dans la base pour recuperer les X derniers et les afficher ici avec leur image
+           ou juste le lien de l'image ex : img/img_article0.png       
+       -->
         </div>
 
         <div class="cellule_activity">
            <div class="backgroundcellules">
             <span class="titleCellules">Activités</span>
             </div>
+        <!-- Contenu des Activités -->
+        <!--         
+           Ici il y'aura le defilement des activités reçu par du AJAX en temps réeel pour que l'on puisse
+           par cette catégorie acceder au lien suivant :
+                -Post facebook sur la page de warten (Bleu foncé)
+                -Tweet sur le twitter de warten  (Bleu cyan)
+                -Photo mise sur instagramm (Jaune soleil)
+                -Articles mis par warten (Rouge)
+                -Lancement d'un live sur twitch (Violet)
+       -->
+        </div>
+        <div class="cellule_biographie">
+           <div class="backgroundcellules">
+                 <span class="titleCellules">Biographie</span>
+            </div>
+        <!-- Contenu de la Biographie -->
+        <!--         
+           Ici il y'aura la biographie de warten qui le presentera sur les grandes lignes avec pas mal
+           d'information comme :
+           - Ses infos personnels (nom, prenom, lieu, age, activité professionnel, sponsor,...)
+           - Ses Structures, son parcours professionnel , son envie de streamer, ses jeux particulier sur lesquels il joue,...
+       -->
         </div>
     </div>
+    <div class="footer_space">
+        
+        
+    </div>
+    
 </div>
+
 
 
 </body>
--- css/config_css.php
+++ css/config_css.php
@@ -1,47 +1,60 @@
 <style>
-    <?php 
-
-    // Theme
-    $colorprimary="#ff0000";
-    $colorsecondary="#000000";
-    $colorbasic="#FFFFFF";
-    $font="font";
-    // Sponsor
-    $tailleSponsor = 13.2;
-    $heightBox=165;
-    $positionSponsor =1;
-    $heightSponsor=60;
-    //Slider
-    $lienSlider=["#","#","#","#"];
-    $nombreSlider=2;
+<?php 
     
-    ?>
-
+    // Theme
+    $colorprimary="#ff0000"; // Rouge
+    $colorsecondary="#000000"; // Noir
+    $colortertiaire="#080808"; // Gris
+    $colorbasic="#FFFFFF"; //Blanc
+    $font="font"; // Police
+    
+    
+    
+    // Sponsor
+    $tailleSponsor=13.2; // Taille en largeur
+    $heightBox=165; // Hauteur de la boite des sponsors
+    $positionSponsor=1; // Position des sponsor
+    $heightSponsor=60; // Hauteur des sponsors
+    
+    
+    
+    //Slider
+    $lienSlider=["#","#","#","#"]; // Lien des sliders
+    $nombreSlider=2; // Nombre d'image pour le sliders
+    
+?>
+    
+    
+    
+    /*  FontFace  */
+    
     @font-face {
         font-family: <?php echo $font ?>;
         src: url("font/hydro.ttf");
     }
     
+    /*  Général  */
     body,
     html {
         margin: 0 auto;
         padding: 0 auto;
         background-color: <?php echo $colorsecondary ?>;
     }
-    
     header {
         height: 40px;
         width: 100%;
         background-color: <?php echo $colorprimary ?>;
     }
-    a{
+    a {
         text-decoration: none;
     }
-    
-    li{
+    li {
         float: left;
     }
-     /*Me suivre*/
+    
+    
+    /*  Me suivre  */
+    
     .container_follow {
         height: 40px;
         float: left;
@@ -53,14 +66,16 @@
         cursor: default;
         font-family: <?php echo $font ?>;
     }
-    
     .container_rs {
         float: left;
         height: 20px;
         margin-top: 5px;
         margin-right: 5px;
     }
-     /*Login & Logout*/
+    
+    
+    /*  Login & Logout  */
+    
     .container_lang {
         float: right;
         margin-right: 65px;
@@ -68,21 +83,18 @@
         margin-left: 15px;
         height: 30px;
     }
-    
     .drapeau {
         margin-top: 7.5px;
         width: 35px;
         height: 25px;
         cursor: pointer;
     }
-    
     .container_log a {
         float: right;
         margin-right: 35px;
         line-height: 40px;
         color: white;
     }
-    
     .container_log a:link {
         text-decoration: none;
         font-family: <?php echo $font ?>;
@@ -90,7 +102,10 @@
         color: white;
         margin-right: 15px;
     }
-     /*Menu & Logo*/
+    
+    
+    /*  Menu & Logo  */
+    
     .contener_menu {
         background-color: <?php echo $colorsecondary ?>;
         height: 90px;
@@ -98,24 +113,20 @@
         border-bottom: 3px <?php echo $colorprimary ?> solid;
         float: left;
     }
-    
     .img_logo {
-        width:150px;
+        width: 150px;
         height: 80px;
         padding-top: 5px;
         margin-left: 80px;
         float: left;
     }
-    
     nav {
         height: 45px;
         width: 490px;
         float: right;
         margin-top: 15px;
     }
-    
     .menu {
-        
         color: <?php echo $colorbasic ?>;
         float: left;
         list-style-type: none;
@@ -124,84 +135,90 @@
         cursor: pointer;
         font-size: 25px;
     }
-    
     .menu:hover {
         color: <?php echo $colorprimary ?>;
     }
-     /*Sponsor*/
-    .sponsor_container{
+    
+    
+    /*  Sponsor  */
+    
+    .sponsor_container {
         height: 165px;
         width: 100%;
-        background-color: #080808;
+        background-color: <?php echo $colortertiaire;
+        ?>;
         float: left;
-        
     }
-    .sponsor_box{
+    .sponsor_box {
         margin: 0 auto;
         width: 1200px;
         height: 165px;
         overflow: hidden;
     }
-    .box{
+    .box {
         height: <?php echo $heightBox?>px;
         width: <?php echo $tailleBox ?>%;
         line-height: 165px;
-        
     }
-    .sponsor{
+    .sponsor {
         height: <?php echo $heightSponsor ?>px;
         margin-top: <?php echo ($heightBox - $heightSponsor)/2?>px;
     }
-    /*Slider*/
-    .slider_container{
+    
+    
+    /*  Slider  */
+    
+    .slider_container {
         width: 100%;
         height: 600px;
         float: left;
         margin: 0 auto;
     }
-    .sliderbox{
+    .sliderbox {
         width: 1200px;
         height: 600px;
         margin: 0 auto;
     }
-  
-    .box_img{
+    .box_img {
         width: 1200px;
         height: 600px;
         position: absolute;
     }
-    .img_slider{
+    .img_slider {
         width: 100%;
         height: 100%;
     }
     
-     /*Cellules*/
-    .cellule_container{
+    
+    /*  Cellules  */
+    
+    .cellule_container {
         float: left;
         position: relative;
         width: 100%;
         height: 800px;
         margin: 0 auto;
         text-align: center;
-        background: black url('img/background_image.png') no-repeat 0 0; 
+        background: black url('img/background_image.png') no-repeat 0 0;
         background-size: 100% 200%;
         background-position: left bottom;
     }
-    .cellule_articles{
-        float:left;
+    .cellule_articles {
+        float: left;
         width: 62.5%;
         height: 650px;
         margin-top: 15px;
         margin-bottom: 15px;
-        background-color: <?php echo $colorsecondary ?>;;
+        background-color: <?php echo $colorsecondary ?>;
+        ;
         text-align: left;
         -moz-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
         -webkit-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
         -o-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
         box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
-        filter:progid:DXImageTransform.Microsoft.Shadow(color=<?php echo $colorbasic ?>, Direction=NaN, Strength=15);
+        filter: progid:DXImageTransform.Microsoft.Shadow(color=<?php echo $colorbasic ?>, Direction=NaN, Strength=15);
     }
-    .cellule_activity{
+    .cellule_activity {
         float: right;
         width: 35%;
         height: 650px;
@@ -213,20 +230,34 @@
         -webkit-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
         -o-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
         box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
-        filter:progid:DXImageTransform.Microsoft.Shadow(color=<?php echo $colorbasic ?>, Direction=NaN, Strength=15);
+        filter: progid:DXImageTransform.Microsoft.Shadow(color=<?php echo $colorbasic ?>, Direction=NaN, Strength=15);
     }
-    .center_image{
+    .cellule_biographie {
+        float: left;
+        width: 100%;
+        height: 650px;
+        margin-top: 15px;
+        margin-bottom: 15px;
+        background-color: <?php echo $colorsecondary ?>;
+        text-align: left;
+        -moz-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        -webkit-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        -o-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        filter: progid:DXImageTransform.Microsoft.Shadow(color=<?php echo $colorbasic ?>, Direction=NaN, Strength=15);
+    }
+    .center_image {
         margin: 0 auto;
         width: 1250px;
     }
-    .backgroundcellules{
+    .backgroundcellules {
         width: 100%;
         height: 50px;
-        background: black url('img/banner_red_cellules.png') no-repeat 0 0; 
+        background: black url('img/banner_red_cellules.png') no-repeat 0 0;
         background-size: 100% 100%;
         vertical-align: center;
     }
-    .titleCellules{
+    .titleCellules {
         line-height: 50px;
         margin-left: 30px;
         font-size: 35px;
@@ -234,5 +265,23 @@
         cursor: default;
         color: <?php echo $colorsecondary ?>;
     }
-</style>
+    
+    
+    /*  Footer  */
+    
+    .footer_space {
+        float: left;
+        width: 100%;
+        height: 250px;
+        margin-top: 150px;
+        background-color: <?php echo $colorsecondary;
+        ?>;
+        border-top: 3px <?php echo $colorprimary ?> solid;
+        -moz-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        -webkit-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        -o-box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        box-shadow: 0px 0px 15px 0px <?php echo $colorbasic ?>;
+        filter: progid:DXImageTransform.Microsoft.Shadow(color=<?php echo $colorbasic ?>, Direction=NaN, Strength=15);
+    }
 
+</style>
