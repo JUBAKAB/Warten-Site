acf259d3ecce4b468b46b18c959fdb9ddc6a0a9e
Charly Stern <charly.strn@gmail.com>
2017-06-14 15:39:40 +0200

    Vues connexions

---
+++ View/Connexion.php
@@ -0,0 +1,11 @@
+
+<form action="index.php?link=Connexion" method="post">
+
+	
+
+	<input type="email" name="mail">
+	<input type="password" name="password">
+	<input type="submit" name="valider" value="Se connecter">
+
+
+</form>
